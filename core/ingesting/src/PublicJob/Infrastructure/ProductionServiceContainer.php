ed07ea5dbeb39d88b6179063e71832cc24d8cbd8
zero <teclaizerai@gmail.com>
2021-12-07 15:00:05 +0100

    Update pjob ProductionServiceContainer.

--- core/ingesting/src/PublicJob/Infrastructure/ProductionServiceContainer.php
+++ core/ingesting/src/PublicJob/Infrastructure/ProductionServiceContainer.php
@@ -2,6 +2,28 @@
 
 namespace Ingesting\PublicJob\Infrastructure;
 
+use Ingesting\PublicJob\Adapter\Persistence\InMemoryJobFeedRepository;
+use Ingesting\PublicJob\Application\Model\JobRepository;
+
 class ProductionServiceContainer extends ServiceContainer
 {
+    protected ?JobRepository $jobRepository = null;
+
+    public function __construct(?JobRepository $jobRepository = null)
+    {
+        $this->jobRepository = $jobRepository;
+    }
+
+    protected function jobRepository(): JobRepository
+    {
+        if ($this->jobRepository === null) {
+            /**
+             * Todo remove InMemoryJobFeedRepository
+             * add Doctrine repository
+             */
+            $this->jobRepository = new InMemoryJobFeedRepository();
+        }
+
+        return $this->jobRepository;
+    }
 }
