ca2c7fd63b06f879aa070251d161d2d86d45940d
zero <teclaizerai@gmail.com>
2021-12-11 12:53:48 +0100

    Added jobRepository contract test (doctrine implementation).

---
+++ core/ingesting/tests/PublicJob/Integration/Persistence/DoctrineJobRepositoryTest.php
@@ -0,0 +1,33 @@
+<?php declare(strict_types=1);
+
+namespace Ingesting\Tests\PublicJob\Integration\Persistence;
+
+use Ingesting\PublicJob\Application\Model\JobRepository;
+
+class DoctrineJobRepositoryTest extends JobRepositoryContractTest
+{
+    /**
+     * @var null|object
+     */
+    private $doctrineJobFeedRepository;
+
+    protected function setUp(): void
+    {
+        $kernel = parent::bootKernel();
+
+        $this->doctrineJobFeedRepository = $kernel->getContainer()
+            ->get('Ingesting\PublicJob\Adapter\Persistence\Doctrine\DoctrineJobFeedRepository');
+
+        parent::setUp();
+    }
+
+    protected function createRepository(): JobRepository
+    {
+        return $this->doctrineJobFeedRepository;
+    }
+
+    protected function tearDown(): void
+    {
+        $this->doctrineJobFeedRepository = null;
+    }
+}
