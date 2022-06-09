e71663ec5f9bcae73239cfa67be05780703aa033
zero <teclaizerai@gmail.com>
2022-06-09 19:10:35 +0200

    [Tools] update phpunit core conf.

---
+++ core/backoffice/tests/Integration/PlaceholderTest.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Backoffice\Tests\Integration;
+
+use PHPUnit\Framework\TestCase;
+
+class PlaceholderTest extends TestCase
+{
+    public function testName(): void
+    {
+        self::assertTrue(true);
+    }
+}
---
+++ core/backoffice/tests/Unit/PlaceholderTest.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Backoffice\Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class PlaceholderTest extends TestCase
+{
+    public function testName(): void
+    {
+        self::assertTrue(true);
+    }
+}
