0d6a00345e3b3beb5b1f292d246e973b99fb0887
zerai <teclaizerai@gmail.com>
2022-06-10 19:59:55 +0200

    Front: added bootstrap 5, webpack conf.

--- core/publishing/assets/app_front.js
+++ core/publishing/assets/app_front.js
@@ -1,12 +1,11 @@
 /*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
+ * App's main JavaScript file for public frontend!
  */
 
-// any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.css';
+import './styles/app.scss';
+
+// import bootstrap 5.1.3
+import * as bootstrap from 'bootstrap';
 
 // start the Stimulus application
 import './bootstrap';
