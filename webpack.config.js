5429f826c14d1cffe28038dede006de63207349a
zerai <teclaizerai@gmail.com>
2022-06-11 13:57:03 +0200

    [Tools] changed webpack conf. separate build (front|backoffice).

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const Encore = require('@symfony/webpack-encore');
+const path = require('path');
 
 // Manually configure the runtime environment if not already configured yet by the "encore" command.
 // It's useful when you use tools that rely on webpack.config.js file.
@@ -6,11 +7,12 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
     Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev');
 }
 
+// define the webpack configuration for front application
 Encore
     // directory where compiled assets will be stored
-    .setOutputPath('public/build/')
+    .setOutputPath('public/build-front/')
     // public path used by the web server to access the output path
-    .setPublicPath('/build')
+    .setPublicPath('/build-front')
     // only needed for CDN's or sub-directory deploy
     //.setManifestKeyPrefix('build/')
 
@@ -20,19 +22,9 @@ Encore
      * Each entry will result in one JavaScript file (e.g. app.js)
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
-    .addEntry('app', './assets/app.js')
-    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    .enableStimulusBridge('./assets/controllers.json')
-
-
     .addEntry('app_front', './core/publishing/assets/app_front.js')
     // enables the Symfony UX Stimulus bridge (used in core/publishing/assets/bootstrap.js)
     .enableStimulusBridge('./core/publishing/assets/controllers.json')
-
-
-    .addEntry('app_backoffice', './core/backoffice/assets/app_backoffice.js')
-    // enables the Symfony UX Stimulus bridge (used in core/publishing/assets/bootstrap.js)
-    .enableStimulusBridge('./core/backoffice/assets/controllers.json')
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -40,18 +32,15 @@ Encore
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
     .enableSingleRuntimeChunk()
+    //.disableSingleRuntimeChunk()
 
     /*
-     * FEATURE CONFIG
-     *
-     * Enable & configure other features below. For a full
      * list of features, see:
      * https://symfony.com/doc/current/frontend.html#adding-more-features
      */
     .cleanupOutputBeforeBuild()
     .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
-    // enables hashed filenames (e.g. app.abc123.css)
     .enableVersioning(Encore.isProduction())
 
     .configureBabel((config) => {
@@ -64,8 +53,14 @@ Encore
         config.corejs = 3;
     })
 
-    // enables Sass/SCSS support
     .enableSassLoader()
+
+    .enablePostCssLoader()
+
+    .copyFiles({
+        from: './core/publishing/assets/images',
+        to: 'images/[path][name].[hash:8].[ext]',
+    })
 
     // uncomment if you use TypeScript
     //.enableTypeScriptLoader()
@@ -79,6 +74,95 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
+
+    // Load bootstrap-icons
+    // .addLoader({
+    //     test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+    //     include: path.resolve(__dirname, './node_modules/bootstrap-icons/font/fonts'),
+    //     use: {
+    //         loader: 'file-loader',
+    //         options: {
+    //             name: '[name].[ext]',
+    //             outputPath: 'webfonts',
+    //             publicPath: '../webfonts',
+    //         },
+    //     }
+    // })
 ;
 
-module.exports = Encore.getWebpackConfig();
+// build the webpack configuration for front application
+const frontAppConfig = Encore.getWebpackConfig();
+// Set a unique name for front application
+frontAppConfig.name = 'frontAppConfig';
+
+
+// reset Encore to build the second config
+Encore.reset();
+
+// define the webpack configuration for backoffice application
+Encore
+    // directory where compiled assets will be stored
+    .setOutputPath('public/build-backoffice/')
+    // public path used by the web server to access the output path
+    .setPublicPath('/build-backoffice')
+    // only needed for CDN's or sub-directory deploy
+    //.setManifestKeyPrefix('build/')
+
+    /*
+     * ENTRY CONFIG
+     *
+     * Each entry will result in one JavaScript file (e.g. app.js)
+     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
+     */
+    .addEntry('app_backoffice', './core/backoffice/assets/app_backoffice.js')
+    //enables the Symfony UX Stimulus bridge (used in core/publishing/assets/bootstrap.js)
+    .enableStimulusBridge('./core/backoffice/assets/controllers.json')
+
+    // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
+    .splitEntryChunks()
+
+    // will require an extra script tag for runtime.js
+    // but, you probably want this, unless you're building a single-page app
+    .enableSingleRuntimeChunk()
+    //.disableSingleRuntimeChunk()
+
+    /*
+     * list of features, see:
+     * https://symfony.com/doc/current/frontend.html#adding-more-features
+     */
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+
+    .configureBabel((config) => {
+        config.plugins.push('@babel/plugin-proposal-class-properties');
+    })
+
+    // enables @babel/preset-env polyfills
+    .configureBabelPresetEnv((config) => {
+        config.useBuiltIns = 'usage';
+        config.corejs = 3;
+    })
+
+    .enableSassLoader()
+
+    .enablePostCssLoader()
+
+    .copyFiles({
+        from: './core/backoffice/assets/images',
+        to: 'images/[path][name].[hash:8].[ext]',
+    })
+
+;
+
+// build the webpack configuration for backoffice application
+const backofficeAppConfig = Encore.getWebpackConfig();
+// Set a unique name for backoffice application
+backofficeAppConfig.name = 'backofficeAppConfig';
+
+
+
+
+// export both configuration (front,backoffice) as an array of multiple configurations
+module.exports = [frontAppConfig, backofficeAppConfig];
