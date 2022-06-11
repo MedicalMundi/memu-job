ce25f7c94ce77dd810ce5adab6dea47a7aa434aa
zerai <teclaizerai@gmail.com>
2022-06-11 14:00:17 +0200

    Improved ui setup for front and backoffice.

--- core/publishing/assets/app_front.js
+++ core/publishing/assets/app_front.js
@@ -3,6 +3,11 @@
  */
 
 import './styles/app.scss';
+//import "bootstrap-icons/font/bootstrap-icons.css";
+//require('bootstrap-icons/font/bootstrap-icons.css');
+//import icon from 'bootstrap-icons/font/bootstrap-icons';
+
+//import '~bootstrap-icons/font/bootstrap-icons';
 
 // import bootstrap 5.1.3
 import * as bootstrap from 'bootstrap';
