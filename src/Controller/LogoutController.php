ec674cc21827c202bcdb49243552402c507fc898
zerai <teclaizerai@gmail.com>
2022-06-13 10:05:27 +0200

    Setup security conf, added login,logut.

---
+++ src/Controller/LogoutController.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LogoutController extends AbstractController
+{
+    /**
+     * @Route("/logout", name="app_logout")
+     */
+    public function index(): void
+    {
+    }
+}
