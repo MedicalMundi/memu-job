da41e6abfc80a004812c86a6270e6d31a5a57745
zerai <teclaizerai@gmail.com>
2022-06-14 22:57:41 +0200

    Update security rules.

--- core/publishing/src/Cms/Adapter/HttpWeb/FrontHomeController.php
+++ core/publishing/src/Cms/Adapter/HttpWeb/FrontHomeController.php
@@ -2,6 +2,7 @@
 
 namespace Publishing\Cms\Adapter\HttpWeb;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,6 +11,7 @@ class FrontHomeController extends AbstractController
 {
     /**
      * @Route("/home", name="app_home")
+     * @IsGranted("ROLE_ADMIN")
      */
     public function index(): Response
     {
