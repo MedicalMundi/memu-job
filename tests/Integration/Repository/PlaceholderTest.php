87ac154098e4ef79fcca15a3ce39088231a23f92
zerai <teclaizerai@gmail.com>
2022-06-23 22:36:40 +0200

    [CI] improve sonar report.

---
+++ tests/Integration/Repository/PlaceholderTest.php
@@ -0,0 +1,16 @@
+<?php declare(strict_types=1);
+
+namespace App\Tests\Integration\Repository;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @group io-database
+ */
+class PlaceholderTest extends TestCase
+{
+    public function testName(): void
+    {
+        self::assertTrue(true);
+    }
+}
