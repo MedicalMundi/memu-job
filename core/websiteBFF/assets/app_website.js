7789bb3ce796739d29a1cbed0a1563637ed05497
zero <teclaizerai@gmail.com>
2023-06-08 14:27:55 +0200

    Update webpack|encore conf. websiteBFF.

---
+++ core/websiteBFF/assets/app_website.js
@@ -0,0 +1,16 @@
+/*
+ * App's main JavaScript file for public frontend!
+ */
+
+import './styles/app.scss';
+//import "bootstrap-icons/font/bootstrap-icons.css";
+//require('bootstrap-icons/font/bootstrap-icons.css');
+//import icon from 'bootstrap-icons/font/bootstrap-icons';
+
+//import '~bootstrap-icons/font/bootstrap-icons';
+
+// import bootstrap 5.1.3
+import * as bootstrap from 'bootstrap';
+
+// start the Stimulus application
+import './bootstrap';
